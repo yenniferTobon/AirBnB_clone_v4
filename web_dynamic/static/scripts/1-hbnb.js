0f818d82f977dd4916d0651856d88700f3e26b2e
joshua <joshuaciencia2016@gmail.com>
2020-02-16 16:11:17 +0000

    task_100

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+let list = {};
+$( document ).ready(function() {
+  let li_selector = 'div.amenities ul.popover li';
+  let l = $(li_selector + ' input:checkbox').each(function(index){
+    $(this).click(function() {
+      let dataId = $(this).parent().attr('data-id');
+      if ($(this).prop('checked')) {
+        list[dataId] = $(this).parent().text();
+      } else {
+        delete list[dataId];
+      }
+      console.log(list);
+      let h4 = $('div.amenities h4');
+      h4.text('');
+      for (let key in list) {
+        h4.text(h4.text() + list[key] + ', ');
+      }
+    });
+  });
+});
